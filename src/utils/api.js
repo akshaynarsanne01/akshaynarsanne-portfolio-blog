8b27fdff7354d8ca35da288d18fd809ff25e7ec6
Akshay Narsanne <147087536+akshaynarsanne01@users.noreply.github.com>
2024-12-03 22:49:41 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://akshaynarsanne-portfolio-1.onrender.com/api' });
+const API = axios.create({ baseURL: 'https://blogapplication-oajc.onrender.com/api' });
 
 // Interceptor for including token in requests
 API.interceptors.request.use((req) => {
